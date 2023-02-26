b1ac731b2669ae94aa5a95a220e041afc185444a
saramsw <saramsoftware@gmail.com>
2023-02-27 00:26:02 +0900

    add uuidSaved

--- lib/kowanas_uuid.dart
+++ lib/kowanas_uuid.dart
@@ -1,5 +1,19 @@
 import 'package:uuid/uuid.dart';
 
+import 'preference/preference.dart';
+
 class KowanasUUID{
   static get uuid => Uuid().v4();
+
+  static get uuidSaved async {
+    final pref = await Preference.instance;
+    String? uuid;
+    if (pref.containsKey('uuid') == true) {
+      uuid = pref.getString('uuid');
+    }else{
+      uuid = KowanasUUID.uuid;
+      pref.setString('uuid', uuid);
+    }
+    return uuid;
+  }
 }
